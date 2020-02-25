9620470831aad05b679e1fce58598a32a5edf0df
Jahvier <50281954+TheSourceBreaker@users.noreply.github.com>
2020-02-24 17:55:33 -0800

    Update

--- Assets/Node.cs
+++ Assets/Node.cs
@@ -9,18 +9,33 @@ public class Node : MonoBehaviour
     public float difficulty;
     [HideInInspector]
     public float gScore;
+    public float hScore;
+    public float fScore
+    {
+        get
+        {
+            return gScore + hScore;
+        }
+    }
 
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.green;
-        for(int i = 0; i < neighbors.Count; i++)
+        for (int i = 0; i < neighbors.Count; i++)
         {
             Gizmos.DrawLine(transform.position, neighbors[i].transform.position);
         }
     }
-    
-    void Update()
+
+    private void OnMouseDown() // change the Beginning Node on mouse click
     {
         
     }
+
+    void Start()
+    {
+        PathFinder.allNodes.Add(this);
+    }
+
+
 }
--- Assets/PathFinder.cs
+++ Assets/PathFinder.cs
@@ -4,43 +4,124 @@ using UnityEngine;
 
 public class PathFinder : MonoBehaviour
 {
-    Node node;
-    List<Node> OpenList;
+    Node node; // References an existing class
+    List<Node> OpenList; // This references an already existing List of type Node
     List<Node> CloseList;
+    List<Node> routeBack = new List<Node>();
 
+    public static List<Node> allNodes = new List<Node>();
+
+    public Vector3 target;
+    public Vector3 startPos;
+
+    public float speed;
 
 
     public void Path(Node Begin, Node End)
     {
-        Node currentNode;
-        while (Begin != End)
-        {
-            currentNode = OpenList[0]; // current node is equal to the node at the start of OpenList
+        OpenList = new List<Node>();
+        CloseList = new List<Node>();
 
-            for (int i = 1; i < OpenList.Count; i++) // OpenList.count = OpenList.size
+        Node currentNode = Begin;
+        OpenList.Add(Begin);
+
+        while (currentNode != End) // fscore and sorting
+        {
+            currentNode = OpenList[0]; // current node is equal to the node at the start of OpenList (for each Node)
+
+            for (int i = 1; i < OpenList.Count; i++) // This loop sorts and finds the lowest fScore inside the list and equals it to the currNode
             {
-                if(OpenList[i].gScore < currentNode.gScore) // Reads through the list and compares its gScore with the currentNode
+                if(OpenList[i].fScore < currentNode.fScore) // Reads through the node inside the list and compares its gScores with the currentNode's gscore
                 {
                     currentNode = OpenList[i]; // If true, then current node is = to the specific list spot
                 }
             }
+
+            OpenList.Remove(currentNode);
             CloseList.Add(currentNode); // Add currentNode to ClosedList
-            node.prevNode = currentNode; // set prevNode to be = to currentNode
+            
+            for (int i = 0; i < currentNode.neighbors.Count; i++) // This loop calculates the neighbor's gScore and hScore using Distance
+            {
+                if(!CloseList.Contains(currentNode.neighbors[i]))
+                {
+                    currentNode.neighbors[i].gScore = currentNode.gScore + Vector3.Distance(currentNode.transform.position, currentNode.neighbors[i].transform.position);//gScore
+                    currentNode.neighbors[i].hScore = Vector3.Distance(currentNode.transform.position, End.transform.position);//hScore
+
+                    currentNode.neighbors[i].prevNode = currentNode; // currentNode is now the previous Node of the neighbor
+                    OpenList.Add(currentNode.neighbors[i]);
+                }
+                else
+                    Debug.Log("Closelist contains neighbor" + "[" + i + "]");
+            }
         }
+
+        while(currentNode != Begin)
+        {
+            routeBack.Add(currentNode);
+            currentNode = currentNode.prevNode;
+        }
+        routeBack.Reverse();
     }
     
 
-    // Start is called before the first frame update
-    void Start()
+    public void FindNewPath( Vector3 endPos)
     {
-        for (int i = 0; i < node.neighbors.Count; i++)
-            OpenList.Add(node.neighbors[0]);
+        Node startNode = allNodes[0];
+        Node endNode = allNodes[0];
+
+        float minStartDist = 0;
+        float maxEndDist = 0;
+
+        foreach (Node node in allNodes) // for each element inside the allNodes 
+        {
+            float startDistance = Vector3.Distance(transform.position, node.transform.position); // checks the min and max of the startPos and endPos
+            float endDistance = Vector3.Distance(endPos, node.transform.position);
+
+
+            if (startDistance > minStartDist) // if 0 is < the min distance
+            {
+                startNode = node;
+                minStartDist = startDistance;
+            }
+
+            if(endDistance > maxEndDist) // if 0 is < the max distance
+            {
+                endNode = node;
+                maxEndDist = endDistance;
+            }
+        }
+        Path(startNode, endNode);
     }
 
-    // Update is called once per frame
+    public void BackTrack()
+    {
+        for(int i = 0; i < CloseList.Count; i++)
+        {
+            CloseList[i].transform.position = transform.position;
+        }
+    }
+
+    void Start()
+    {
+
+    }
+
     void Update()
     {
-        Path(Begin,End);
-        
+        Vector3 v = new Vector3();
+        //while (v != routeBack.)
+        //{
+
+        //}
+
+        for(int i = 0; i < routeBack.Count; i++)
+        {
+            if(v == routeBack[i].transform.position)
+            {
+                routeBack[i]
+
+            }
+        }
+
     }
 }
